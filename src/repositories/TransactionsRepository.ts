1ffd75312d2090a9ab3bdec5e80ba68289fa1150
Duany Dreyton <duanyrf@gmail.com>
2020-11-01 15:33:55 -0300

    finish Level 2 Challenge 5

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce(
+      (accumulator, currentValue) =>
+        currentValue.type === 'income'
+          ? accumulator + currentValue.value
+          : accumulator,
+      0,
+    );
+
+    const outcome = this.transactions.reduce(
+      (accumulator, currentValue) =>
+        currentValue.type === 'outcome'
+          ? accumulator + currentValue.value
+          : accumulator,
+      0,
+    );
+
+    const total = income - outcome;
+
+    const balance = { income, outcome, total };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
